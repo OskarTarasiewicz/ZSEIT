4b54f81faece6a04a2fc7e41ab717a8da7e081d8
Oskar <99892836+OskarTarasiewicz@users.noreply.github.com>
2023-02-27 18:52:57 +0100

    Update

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,33 +1,43 @@
-import React, { useEffect, useState } from "react";
-import { Box, Heading, Select, Textarea } from '@chakra-ui/react';
-import { NextPage } from "next";
+import React, {Suspense, useEffect, useState} from "react";
+import {Box, Heading, Select, Textarea} from '@chakra-ui/react';
+import {NextPage} from "next";
 import axios from "axios";
 
 const Home: NextPage = () => {
-  const [data, setData] = useState();
+    const [data, setData] = useState([]);
 
-  useEffect(() => {
-    axios.get('http://localhost:3001/api/read', {withCredentials: true})
-    .then(function(response) {
-      if (response.status === 200) {
-        setData(response.data.msg)
-        console.log(response.data.msg)
-      }
-    })
-  });
+    useEffect(() => {
+        axios.get('http://localhost:3001/api/all', {
+            withCredentials: true
+        }).then(function (response) {
+            if (response.status === 200) {
+                response.data.msg.forEach((n: any) => {
+                    // @ts-ignore
+                    return data.push(n);
+                })
+                console.log(data)
+            }
+        }).catch(err => {
+            alert(err);
+        })
+    }, []);
 
-  return (
-    <Box bg={'gray.800'} width={'100vw'} height={'98vh'}>
-      <Heading padding={'12'} textAlign={'center'} color={'white'}>(READ) - Podgląd plików</Heading>
-      <Box padding={'32'} display={'flex'} flexDir={'column'} justifyContent={'center'}>
-        <Select variant={'filled'}>
-          <option>notatka</option>
-          <option>testowa treść</option>
-        </Select>
-        <Textarea variant={'filled'} marginTop={'24'} disabled />
-      </Box>
-    </Box>
-  );
+    return (
+        <Box bg={'gray.800'} width={'100vw'} height={'98vh'}>
+            <Heading padding={'12'} textAlign={'center'} color={'white'}>(READ) - Podgląd plików</Heading>
+            <Box padding={'32'} display={'flex'} flexDir={'column'} justifyContent={'center'}>
+                <Select variant={'filled'}>
+                    <Suspense fallback={'Ładowanie...'}>
+                        {data.map((value, i) => {
+                            // @ts-ignore
+                            return (<option key={i} value={value.title}>{value.title}</option>);
+                        })}
+                    </Suspense>
+                </Select>
+                <Textarea variant={'filled'} marginTop={'24'} disabled/>
+            </Box>
+        </Box>
+    );
 };
 
 export default Home;
